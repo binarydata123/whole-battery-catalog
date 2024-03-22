74d6a32655835c427b7390e4828248c6998678dc
sunil-des <binarydata123>
2024-03-22 16:11:09 +0530

    code add

--- client/src/components/Providers/index.tsx
+++ client/src/components/Providers/index.tsx
@@ -17,7 +17,6 @@ const Providers = (props: Props) => {
 	const pathname = usePathname();
 
 	const { user } = useContext(AuthContext);
-	const [isMobile, setIsMobile] = useState(false);
 	const excludedPaths = [
 		'/admin',
 		'/staff',
@@ -29,28 +28,11 @@ const Providers = (props: Props) => {
 		'/payment'
 	];
 
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth <= 767);
-		};
-
-		// Initial check
-		handleResize();
-
-		// Event listener for window resize
-		window.addEventListener('resize', handleResize);
-
-		// Cleanup function to remove event listener
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
-
 	return (
 		<SessionProvider>
 			{excludedPaths.some((path) => pathname.includes(path)) ? null : <Header />}
 			{props.children}
-			{excludedPaths.some((path) => pathname.includes(path)) || isMobile ? '' : <Footer />}
+			{excludedPaths.some((path) => pathname.includes(path)) ? null : <Footer />}
 		</SessionProvider>
 	);
 };
